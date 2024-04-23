a981a879ba93e3ab95de3f32a83c39f276f79fc9
S25088 <S25088@pjwstk.edu.pl>
2024-04-23 11:20:11 +0200

    implementacja metod z interface

--- AnimalsDbConnection/Repositores/AnimalsRepository.cs
+++ AnimalsDbConnection/Repositores/AnimalsRepository.cs
@@ -1,6 +1,34 @@
-﻿namespace AnimalsDbConnection.Repositores;
+﻿using AnimalsDbConnection.Services;
 
-public class AnimalsRepository
+namespace AnimalsDbConnection.Repositores;
+
+public class AnimalsRepository : IAnimalsService
 {
+    private readonly IAnimalsRepository _animalsRepository;
+
+    public AnimalsService(IAnimalsRepository animalsRepository)
+    {
+        _animalsRepository = animalsRepository;
+    }
+    public IEnumerable<Animal> GetAnimals(string orderBy)
+    {
+        return _animalsRepository.GetAnimals(orderBy);
+    }
+
+    public int AddAnimal(Animal animal)
+    {
+        return _animalsRepository.AddAnimal(animal);
+    }
+
+    public int UpdateAnimal(int id,Animal animal)
+    {
+        return _animalsRepository.UpdateAnimal(id, animal);
+    }
+
+    public int DeleteAnimal(int idAnimal)
+    {
+        return _animalsRepository.DeleteAnimal(idAnimal);
+    }
+    
     
 }

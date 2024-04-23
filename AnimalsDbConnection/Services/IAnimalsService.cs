9376ca493179ab7f42c2cf88664f1a2364db80dc
S25088 <S25088@pjwstk.edu.pl>
2024-04-23 11:07:26 +0200

    dodanie metod w interface

--- AnimalsDbConnection/Services/IAnimalsService.cs
+++ AnimalsDbConnection/Services/IAnimalsService.cs
@@ -1,6 +1,8 @@
-﻿namespace AnimalsDbConnection.Services;
-
+﻿
 public interface IAnimalsService
 {
-    
+    IEnumerable<Animal> GetAnimals(string orderBy);
+    int AddAnimal(Animal animal);
+    int UpdateAnimal(int id, Animal animal);
+    int DeleteAnimal(int idAnimal);
 }

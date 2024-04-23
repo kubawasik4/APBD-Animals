81b809d9f35fd352673a253387f042f9fbeebd17
S25088 <S25088@pjwstk.edu.pl>
2024-04-23 11:09:39 +0200

    dodanie metod w interface

--- AnimalsDbConnection/Repositores/IAnimalsRepository.cs
+++ AnimalsDbConnection/Repositores/IAnimalsRepository.cs
@@ -2,5 +2,8 @@
 
 public interface IAnimalsRepository
 {
-    
+    IEnumerable<Animal> GetAnimals(string orderBy);
+    int AddAnimal(Animal animal);
+    int UpdateAnimal(int id, Animal animal);
+    int DeleteAnimal(int idAnimal);
 }

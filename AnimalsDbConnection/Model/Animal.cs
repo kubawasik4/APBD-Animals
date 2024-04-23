5ec1d2d36ccd459023e62911aeaf9cd644cc3b30
S25088 <S25088@pjwstk.edu.pl>
2024-04-23 11:04:03 +0200

    get set model

--- AnimalsDbConnection/Model/Animal.cs
+++ AnimalsDbConnection/Model/Animal.cs
@@ -1,6 +1,13 @@
-﻿namespace AnimalsDbConnection.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Model;
 
 public class Animal
 {
-    
+    public int IdAnimal { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string Area { get; set; }
+   
 }

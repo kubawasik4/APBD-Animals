28fdb86556c903c338d90a67edf095e3a971a4da
S25088 <S25088@pjwstk.edu.pl>
2024-04-24 12:53:56 +0200

    Implementacja metody addAnimal

--- AnimalsDbConnection/Repositores/AnimalsRepository.cs
+++ AnimalsDbConnection/Repositores/AnimalsRepository.cs
@@ -1,4 +1,5 @@
-﻿using AnimalsDbConnection.Services;
+﻿using System.Data.SqlClient;
+using AnimalsDbConnection.Services;
 
 namespace AnimalsDbConnection.Repositores;
 
@@ -13,7 +14,36 @@ public class AnimalsRepository : IAnimalsRepository
     }
     public IEnumerable<Animal> GetAnimals(string orderBy)
     {
-        return _animalsRepository.GetAnimals(orderBy);
+        var animals = new List<Animal>();
+        
+        using (var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]))
+        {
+            connection.Open();
+
+            using (var cmd = new SqlCommand())
+            {
+                cmd.Connection = connection;
+                var safeOrderBy = new string[] { "Name", "Description", "Category", "Area" }.Contains(orderBy) ? orderBy : "Name";
+                cmd.CommandText = $"SELECT IdAnimal, Name, Description, Category, Area FROM Animal ORDER BY {safeOrderBy}";
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var animal = new Animal
+                    {
+                        IdAnimal = (int)reader["IdAnimal"],
+                        Name = reader["Name"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        Category = reader["Category"].ToString(),
+                        Area = reader["Area"].ToString()
+                    };
+                    animals.Add(animal);
+                }
+            }
+        }
+
+        return animals;
     }
 
     public int AddAnimal(Animal animal)
--- AnimalsDbConnection/obj/Debug/net8.0/AnimalsDbConnection.AssemblyInfo.cs
+++ AnimalsDbConnection/obj/Debug/net8.0/AnimalsDbConnection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimalsDbConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77fed0f7e85a328c579074561ef63d65f7f7d104")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bb9da1b0f9cd557212e40d79a91ded46f5e396")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimalsDbConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimalsDbConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

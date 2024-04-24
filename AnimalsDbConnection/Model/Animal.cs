96d98c126144d4391e12481236141a4aa5c36ce0
S25088 <S25088@pjwstk.edu.pl>
2024-04-24 13:12:13 +0200

    dodanie walidacji pól typu string

--- AnimalsDbConnection/Model/Animal.cs
+++ AnimalsDbConnection/Model/Animal.cs
@@ -1,18 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using AnimalsDbConnection.Validators;
 
 public class Animal
 {
     public int IdAnimal { get; set; }
     [Required]
     [MaxLength(50)]
+    [StringValidator(ErrorMessage = "pole Name nie moze miec nazwy 'string'")]
     public string Name { get; set; }
     [Required]
     [MaxLength(200)]
+    [StringValidator(ErrorMessage = "pole Description nie moze miec nazwy 'string'")]
     public string Description { get; set; }
     [Required]
+    [MaxLength(20)]
+    [StringValidator(ErrorMessage = "pole Category nie moze miec nazwy 'string'")]
     public string Category { get; set; }
     [Required]
     [MaxLength(20)]
+    [StringValidator(ErrorMessage = "pole Area nie moze miec nazwy 'string'")]
     public string Area { get; set; }
    
 }

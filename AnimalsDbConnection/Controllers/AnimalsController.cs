da6ed222033da18539954ee1ffc2d60228e2c50b
S25088 <S25088@pjwstk.edu.pl>
2024-04-24 12:32:39 +0200

    wstrzykniecei zaleznosci i obsluga http

--- AnimalsDbConnection/Controllers/AnimalsController.cs
+++ AnimalsDbConnection/Controllers/AnimalsController.cs
@@ -1,6 +1,14 @@
-﻿namespace AnimalsDbConnection.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
 
-public class AnimalsController
+namespace AnimalsDbConnection.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AnimalsController : ControllerBase
 {
-    
+    private IAnimalsService _animalsService;
+
+    public AnimalsController(IAnimalsService animalsService)
+    {
+        _animalsService = animalsService;
+    }
 }
